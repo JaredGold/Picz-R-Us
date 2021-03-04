76e2a4833977d61edc7192ebcc275b65137c77b9
JaredGold <jegoldstein94@gmail.com>
2021-03-04 14:47:37 +1100

    Add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  # localhost:3000/
-  root "home#index"
+  devise_for :users, controllers: { registrations: "registrations" }
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+  end
+
+
 end
