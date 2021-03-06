43a215c8e3cbd2f0fef33727433c90d564d5ab93
JaredGold <jegoldstein94@gmail.com>
2021-03-06 21:08:48 +1100

    Add profile page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,9 @@
 class ProfileController < ApplicationController
+before_action :authenticate_user!, only: %i[show]
+
   def show
-  
+    @user = current_user
+    @name = "#{@user.first_name} #{@user.last_name}"
+    @profile = current_user.profile
   end
 end
