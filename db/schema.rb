f5657470348fe445de6a5662443458c16e489256
JaredGold <jegoldstein94@gmail.com>
2021-03-06 19:46:03 +1100

    Add model profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_030511) do
+ActiveRecord::Schema.define(version: 2021_03_06_084451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 2021_03_05_030511) do
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "about_me"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -79,7 +88,6 @@ ActiveRecord::Schema.define(version: 2021_03_05_030511) do
     t.string "username"
     t.string "first_name"
     t.string "last_name"
-    t.text "about_me"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -88,4 +96,5 @@ ActiveRecord::Schema.define(version: 2021_03_05_030511) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "listings", "types"
   add_foreign_key "listings", "users"
+  add_foreign_key "profiles", "users"
 end
