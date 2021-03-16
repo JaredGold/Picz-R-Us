7e61e40f01381bf4cb76d9ff8af920fc642cb1f1
JaredGold <jegoldstein94@gmail.com>
2021-03-16 14:19:18 +1100

    Add styling to forms

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,14 +1,14 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: %i[show edit update destroy find_seller]
-  before_action :find_seller, :find_type, only: %i[show]
+  before_action :find_seller, only: %i[show]
   before_action :authenticate_user!, only: %i[new]
   before_action :set_card, only: %i[index]
+  
 
   # GET /listings or /listings.json
   def index
     @listings = Listing.all
     @user = User.all
-    @type = Type.all
   end
 
   # GET /listings/1 or /listings/1.json
@@ -47,8 +47,8 @@ class ListingsController < ApplicationController
 
   # POST /listings or /listings.json
   def create
+    # accepted_formats = ['.jpeg', '.jpg', '.png', '.tiff', '.bmp', '.gif', '.raw']
     @listing = current_user.listings.create(listing_params)
-    # @listing.footage.attach()
 
     respond_to do |format|
       if @listing.save
@@ -97,10 +97,6 @@ class ListingsController < ApplicationController
     # Find listing owner
     def find_seller
       @seller = User.find(@listing.user_id)
-    end
-
-    def find_type
-      @type = Type.find(@listing.type_id)
     end
 
     def set_card
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,4 @@
 class Listing < ApplicationRecord
   belongs_to :user
-  belongs_to :type
   has_one_attached :footage
 end
